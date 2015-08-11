8e52de8c60c657eb8bcf3579421b79f9b1cf54f4
Anuja Ware <anuja@joshsoftware.com>
2015-08-11 18:30:21 +0530

    Indentation

--- js/AppView.js
+++ js/AppView.js
@@ -1,139 +1,139 @@
 define([
-	'backbone',
-	'views/ui/PaletteView',
-	'views/ui/MintModalView',
-	'css!/css/AppView.css'
+    'backbone',
+    'views/ui/PaletteView',
+    'views/ui/MintModalView',
+    'css!/css/AppView.css'
 ], function(
-	Backbone,
-	PaletteView,
-	MintModalView
+    Backbone,
+    PaletteView,
+    MintModalView
 ) {
 
-	var Router = Backbone.Router.extend({
-		initialize: function() {
-			this.routesHit = 0;
-			//keep count of number of routes handled by your application
-			Backbone.history.on('route', function() {
-				this.routesHit++;
-			}, this);
-		},
+    var Router = Backbone.Router.extend({
+        initialize: function() {
+            this.routesHit = 0;
+            //keep count of number of routes handled by your application
+            Backbone.history.on('route', function() {
+                this.routesHit++;
+            }, this);
+        },
 
-		back: function(options) {
-			var settings = _.extend({
-					trigger: true,
-					replace: true
-				}, options);
-			
-			if (this.routesHit > 1) {
-				window.history.back();
-			} else {
-				this.navigate('', settings);
-			}
-		},
+        back: function(options) {
+            var settings = _.extend({
+                trigger: true,
+                replace: true
+            }, options);
 
-		getCurrentRoute: function() {
-			return this.routes[Backbone.history.getFragment()]
-		},
+            if (this.routesHit > 1) {
+                window.history.back();
+            } else {
+                this.navigate('', settings);
+            }
+        },
 
-		routes: {
-			"": "conversation",
-			"conversation": "conversation",
-			"dashboard": "dashboard",
-			"skintoskin": "skintoskin",
-			"rounds": "rounds"
-		}
-	});
+        getCurrentRoute: function() {
+            return this.routes[Backbone.history.getFragment()]
+        },
 
-	var AppView = Backbone.View.extend({
-		tagName: "section",
-		className: "AppView",
-		router: new Router(),
-		currentNav: null,
-		currentView: null,
-		previousView: null,
-		switchView: function(options) {
-			var that = this;
-			this.previousView = this.currentView;
-			this.currentView = new options.view();
-			var createNewView = function() {
-				that.$el.html(that.currentView.render().$el);
-				if (that.currentView.Nav) {
-					that.currentNav = new that.currentView.Nav({
-						attachedToView: that.currentView
-					});
-					that.currentView.navInstance = that.currentNav;
-					that.$el.prepend(that.currentNav.render().$el);
-					that.currentView.$el.addClass((that.currentNav.collapsed) ? 'withNavBarViewCollapsed' : 'withNavBarView');
-				}
-			}
+        routes: {
+            "": "conversation",
+            "conversation": "conversation",
+            "dashboard": "dashboard",
+            "skintoskin": "skintoskin",
+            "rounds": "rounds"
+        }
+    });
 
-			if (this.previousView) {
-				this.previousView.on('removed', createNewView);
-				this.previousView.remove();
-			} else {
-				createNewView();
-			}
+    var AppView = Backbone.View.extend({
+        tagName: "section",
+        className: "AppView",
+        router: new Router(),
+        currentNav: null,
+        currentView: null,
+        previousView: null,
+        switchView: function(options) {
+            var that = this;
+            this.previousView = this.currentView;
+            this.currentView = new options.view();
+            var createNewView = function() {
+                that.$el.html(that.currentView.render().$el);
+                if (that.currentView.Nav) {
+                    that.currentNav = new that.currentView.Nav({
+                        attachedToView: that.currentView
+                    });
+                    that.currentView.navInstance = that.currentNav;
+                    that.$el.prepend(that.currentNav.render().$el);
+                    that.currentView.$el.addClass((that.currentNav.collapsed) ? 'withNavBarViewCollapsed' : 'withNavBarView');
+                }
+            }
+
+            if (this.previousView) {
+                this.previousView.on('removed', createNewView);
+                this.previousView.remove();
+            } else {
+                createNewView();
+            }
 
 
-		},
-		startRouter: function() {
+        },
+        startRouter: function() {
 
-			var that = this;
+            var that = this;
 
-			this.router.on("route:conversation", function() {
-				require(['views/HomeDialogView'], function(HomeDialogView) {
-					that.switchView({
-						view: HomeDialogView
-					});
-				});
-			});
+            this.router.on("route:conversation", function() {
+                require(['views/HomeDialogView'], function(HomeDialogView) {
+                    that.switchView({
+                        view: HomeDialogView
+                    });
+                });
+            });
 
-			this.router.on("route:dashboard", function() {
-				require(['views/HomeDashboardView'], function(HomeDashboardView) {
-					that.switchView({
-						view: HomeDashboardView,
-					});
-				});
-			});
+            this.router.on("route:dashboard", function() {
+                require(['views/HomeDashboardView'], function(HomeDashboardView) {
+                    that.switchView({
+                        view: HomeDashboardView,
+                    });
+                });
+            });
 
-			this.router.on("route:skintoskin", function() {
-				require(['views/SkinToSkinDetailView'], function(SkinToSkinDetailView) {
-					that.switchView({
-						view: SkinToSkinDetailView,
-					});
-				});
-			});
+            this.router.on("route:skintoskin", function() {
+                require(['views/SkinToSkinDetailView'], function(SkinToSkinDetailView) {
+                    that.switchView({
+                        view: SkinToSkinDetailView,
+                    });
+                });
+            });
 
-			this.router.on("route:rounds", function() {
-				require(['views/RoundsDetailView'], function(RoundsDetailView) {
-					that.switchView({
-						view: RoundsDetailView,
-					});
-				});
-			});
+            this.router.on("route:rounds", function() {
+                require(['views/RoundsDetailView'], function(RoundsDetailView) {
+                    that.switchView({
+                        view: RoundsDetailView,
+                    });
+                });
+            });
 
-			Backbone.history.start();
-		},
-		initialize: function() {
-			this.startRouter();
-			$('body').removeClass('preload');
-		},
-		render: function() {
-			$('body').html(this.$el);
+            Backbone.history.start();
+        },
+        initialize: function() {
+            this.startRouter();
+            $('body').removeClass('preload');
+        },
+        render: function() {
+            $('body').html(this.$el);
 
-			// Show the Palette
-			$('body').append(new PaletteView().render().$el);
+            // Show the Palette
+            $('body').append(new PaletteView().render().$el);
 
-			return this;
-		}
-	}, {
-		getInstance: function() {
-			return instance;
-		}
-	});
+            return this;
+        }
+    }, {
+        getInstance: function() {
+            return instance;
+        }
+    });
 
-	var instance = new AppView();
+    var instance = new AppView();
 
-	return AppView;
+    return AppView;
 
 });
--- js/models/ConversationItemModelCollection.js
+++ js/models/ConversationItemModelCollection.js
@@ -1,232 +1,233 @@
 define([
-	'backbone',
-	'models/ConversationItemModel'
-	], 
-	function(
-		Backbone, 
-		ConversationItemModel
-	){
+        'backbone',
+        'models/ConversationItemModel'
+    ],
+    function(
+        Backbone,
+        ConversationItemModel
+    ) {
 
-	// Everything here would happen on the server
-	var fakeServer = {
+        // Everything here would happen on the server
+        var fakeServer = {
 
-		getVariableDelayTime: function(){
-			var r = Math.floor((Math.random() * (this.fakeServerDelayTime*3)) + this.fakeServerDelayTime);
-			return r;
-		},
+            getVariableDelayTime: function() {
+                var r = Math.floor((Math.random() * (this.fakeServerDelayTime * 3)) + this.fakeServerDelayTime);
+                return r;
+            },
 
-		fakeServerDelayTime : 800,
+            fakeServerDelayTime: 800,
 
-		fakeServerDecideWhichToShow: function(collection){
-			var collection = collection.where({stub: false});
+            fakeServerDecideWhichToShow: function(collection) {
+                var collection = collection.where({
+                    stub: false
+                });
 
-			if(collection.length == 0){
-				return this.fakeDB[0];
-			}
+                if (collection.length == 0) {
+                    return this.fakeDB[0];
+                }
 
-			var lastItem = collection[collection.length-1];
-			var requiresResponseType = lastItem.get('requiresResponseType');
+                var lastItem = collection[collection.length - 1];
+                var requiresResponseType = lastItem.get('requiresResponseType');
 
-			// If the last item was a selection of a CHOICE
-			if(lastItem.get('responseType') == ConversationItemModel.responseType.CHOICE){
-				// get the original question
-				var originalQuestionItem =  lastItem.get('responseTo');
-				var nextId = originalQuestionItem.get('possibleResponses')[lastItem.get('selectedChoice')];
+                // If the last item was a selection of a CHOICE
+                if (lastItem.get('responseType') == ConversationItemModel.responseType.CHOICE) {
+                    // get the original question
+                    var originalQuestionItem = lastItem.get('responseTo');
+                    var nextId = originalQuestionItem.get('possibleResponses')[lastItem.get('selectedChoice')];
 
-				if(typeof(nextId) != "undefined"){
-					var nextItem = _.findWhere(this.fakeDB, {id: nextId});
-					return nextItem;
-				}else{
-					console.error("Could not find "+selectedChoice+" inside possibleResponses arr for ", possibleResponses);
-				}
+                    if (typeof(nextId) != "undefined") {
+                        var nextItem = _.findWhere(this.fakeDB, {
+                            id: nextId
+                        });
+                        return nextItem;
+                    } else {
+                        console.error("Could not find " + selectedChoice + " inside possibleResponses arr for ", possibleResponses);
+                    }
 
-			}
+                }
 
-			// If the last item was an INPUT value given
-			else if(lastItem.get('responseType') == ConversationItemModel.responseType.INPUT){
+                // If the last item was an INPUT value given
+                else if (lastItem.get('responseType') == ConversationItemModel.responseType.INPUT) {
 
-				// Bunch of server side magic...
-				var nextItem = _.findWhere(this.fakeDB, {id: 3});
-				return nextItem;
-			}
+                    // Bunch of server side magic...
+                    var nextItem = _.findWhere(this.fakeDB, {
+                        id: 3
+                    });
+                    return nextItem;
+                } else if (requiresResponseType == ConversationItemModel.responseType.BOTCONTINUE) {
+                    var nextItem = _.findWhere(this.fakeDB, {
+                        id: lastItem.get('possibleResponses')[0]
+                    });
+                    return nextItem;
+                }
 
-			else if(requiresResponseType == ConversationItemModel.responseType.BOTCONTINUE){
-				var nextItem = _.findWhere(this.fakeDB, {id: lastItem.get('possibleResponses')[0]});
-				return nextItem;
-			}
-
-		},
-		// A possible schema for a conversation
-		fakeDB : [
-		{
-			id: 1,
-			responseTo: 21,
-			message: "how are you doing today?",
-			requiresResponseType: ConversationItemModel.responseType.INPUT,
-			responseType: ConversationItemModel.responseType.BOTCONTINUE,
-			choices: ["Meh", "Great!"],
-			possibleResponses: [3,4]
-		},
-		{
-			id: 20,
-			responseTo: 0,
-			message: "By the way, looks like you've been getting in more Skin to Skin time. That's great",
-			requiresResponseType: ConversationItemModel.responseType.BOTCONTINUE,
-			responseType: ConversationItemModel.responseType.BOTCONTINUE,
-			possibleResponses: [21]
-		},
-		{
-			id: 3,
-			responseTo: 2,
-			message: "bummer :(",
-			requiresResponseType: ConversationItemModel.responseType.BOTCONTINUE,
-			responseType: ConversationItemModel.responseType.BOTCONTINUE,
-			possibleResponses: [5]
-		},
-		{
-			id: 4,
-			responseTo: 2,
-			message: "Great!",
-			requiresResponseType: ConversationItemModel.responseType.BOTCONTINUE,
-			responseType: ConversationItemModel.responseType.BOTCONTINUE,
-			possibleResponses: [7]
-		},
-		{
-			id: 5,
-			responseTo: 3,
-			message: "Can I make you a cup of tea?",
-			choices: ["Sure that should help", "How about a coffee instead"],
-			responseType: ConversationItemModel.responseType.BOTCONTINUE,
-			possibleResponses: [8,9],
-			requiresResponseType: ConversationItemModel.responseType.CHOICE,
-		},
-		{
-			id: 7,
-			responseTo: 3,
-			message: "Welp, guess I'll talk to you later then!",
-			responseType: ConversationItemModel.responseType.BOTCONTINUE,
-			requiresResponseType: ConversationItemModel.responseType.NONE,
-		},
-		{
-			id: 8,
-			responseTo: 5,
-			message: "<img src=\"http://pngimg.com/upload/cup_PNG2000.png\" />",
-			responseType: ConversationItemModel.responseType.BOTCONTINUE,
-			requiresResponseType: ConversationItemModel.responseType.BOTCONTINUE,
-			possibleResponses: [10]
-		},
-		{
-			id: 10,
-			responseTo: 8,
-			message: ":D",
-			responseType: ConversationItemModel.responseType.BOTCONTINUE,
-			requiresResponseType: ConversationItemModel.responseType.BOTCONTINUE,
-			possibleResponses: [20]
-		},
-		{
-			id: 9,
-			responseTo: 5,
-			message: "<img src=\"http://www.downeastcoffee.ca/sites/default/files/media/slides/coffee.png\" />",
-			responseType: ConversationItemModel.responseType.BOTCONTINUE,
-			requiresResponseType: ConversationItemModel.responseType.BOTCONTINUE,
-			possibleResponses: [10]
-		},
-		{
-			id: 21,
-			responseTo: 20,
-			message: "{{carditem:skintoskin}}",
-			responseType: ConversationItemModel.responseType.BOTCONTINUE,
-			requiresResponseType: ConversationItemModel.responseType.NONE
-		}
-		]
-	};
+            },
+            // A possible schema for a conversation
+            fakeDB: [{
+                id: 1,
+                responseTo: 21,
+                message: "how are you doing today?",
+                requiresResponseType: ConversationItemModel.responseType.INPUT,
+                responseType: ConversationItemModel.responseType.BOTCONTINUE,
+                choices: ["Meh", "Great!"],
+                possibleResponses: [3, 4]
+            }, {
+                id: 20,
+                responseTo: 0,
+                message: "By the way, looks like you've been getting in more Skin to Skin time. That's great",
+                requiresResponseType: ConversationItemModel.responseType.BOTCONTINUE,
+                responseType: ConversationItemModel.responseType.BOTCONTINUE,
+                possibleResponses: [21]
+            }, {
+                id: 3,
+                responseTo: 2,
+                message: "bummer :(",
+                requiresResponseType: ConversationItemModel.responseType.BOTCONTINUE,
+                responseType: ConversationItemModel.responseType.BOTCONTINUE,
+                possibleResponses: [5]
+            }, {
+                id: 4,
+                responseTo: 2,
+                message: "Great!",
+                requiresResponseType: ConversationItemModel.responseType.BOTCONTINUE,
+                responseType: ConversationItemModel.responseType.BOTCONTINUE,
+                possibleResponses: [7]
+            }, {
+                id: 5,
+                responseTo: 3,
+                message: "Can I make you a cup of tea?",
+                choices: ["Sure that should help", "How about a coffee instead"],
+                responseType: ConversationItemModel.responseType.BOTCONTINUE,
+                possibleResponses: [8, 9],
+                requiresResponseType: ConversationItemModel.responseType.CHOICE,
+            }, {
+                id: 7,
+                responseTo: 3,
+                message: "Welp, guess I'll talk to you later then!",
+                responseType: ConversationItemModel.responseType.BOTCONTINUE,
+                requiresResponseType: ConversationItemModel.responseType.NONE,
+            }, {
+                id: 8,
+                responseTo: 5,
+                message: "<img src=\"http://pngimg.com/upload/cup_PNG2000.png\" />",
+                responseType: ConversationItemModel.responseType.BOTCONTINUE,
+                requiresResponseType: ConversationItemModel.responseType.BOTCONTINUE,
+                possibleResponses: [10]
+            }, {
+                id: 10,
+                responseTo: 8,
+                message: ":D",
+                responseType: ConversationItemModel.responseType.BOTCONTINUE,
+                requiresResponseType: ConversationItemModel.responseType.BOTCONTINUE,
+                possibleResponses: [20]
+            }, {
+                id: 9,
+                responseTo: 5,
+                message: "<img src=\"http://www.downeastcoffee.ca/sites/default/files/media/slides/coffee.png\" />",
+                responseType: ConversationItemModel.responseType.BOTCONTINUE,
+                requiresResponseType: ConversationItemModel.responseType.BOTCONTINUE,
+                possibleResponses: [10]
+            }, {
+                id: 21,
+                responseTo: 20,
+                message: "{{carditem:skintoskin}}",
+                responseType: ConversationItemModel.responseType.BOTCONTINUE,
+                requiresResponseType: ConversationItemModel.responseType.NONE
+            }]
+        };
 
 
-	var ConversationItemModelCollection = Backbone.Collection.extend({
+        var ConversationItemModelCollection = Backbone.Collection.extend({
 
-		addUserResponse: function(userResponse){
-			var reqResponseType = userResponse.responseTo.get('requiresResponseType');
-			userResponse.id = userResponse.responseTo.get('id') + 1;
-			userResponse.fromType = ConversationItemModel.fromType.USER;
-			userResponse.requiresResponseType = ConversationItemModel.responseType.BOTCONTINUE;
+            addUserResponse: function(userResponse) {
+                var reqResponseType = userResponse.responseTo.get('requiresResponseType');
+                userResponse.id = userResponse.responseTo.get('id') + 1;
+                userResponse.fromType = ConversationItemModel.fromType.USER;
+                userResponse.requiresResponseType = ConversationItemModel.responseType.BOTCONTINUE;
 
-			if(reqResponseType == ConversationItemModel.responseType.CHOICE){
-				userResponse.message = userResponse.responseTo.get('choices')[userResponse.selectedChoice];
-			}
-			else if(reqResponseType == ConversationItemModel.responseType.INPUT){
-				userResponse.message = userResponse.responseText;
-			}
+                if (reqResponseType == ConversationItemModel.responseType.CHOICE) {
+                    userResponse.message = userResponse.responseTo.get('choices')[userResponse.selectedChoice];
+                } else if (reqResponseType == ConversationItemModel.responseType.INPUT) {
+                    userResponse.message = userResponse.responseText;
+                }
 
-			this.add(userResponse);
-		},
+                this.add(userResponse);
+            },
 
-		create: function(userResponse){
-			var that = this;
+            create: function(userResponse) {
+                var that = this;
 
-			return $.Deferred(function(dfd){
-				setTimeout(function(){
+                return $.Deferred(function(dfd) {
+                    setTimeout(function() {
 
-					that.fetch();
-					dfd.resolve();
+                        that.fetch();
+                        dfd.resolve();
 
-				},fakeServer.fakeServerDelayTime)
-			});
-		},
+                    }, fakeServer.fakeServerDelayTime)
+                });
+            },
 
-		fetch: function(){
-			// Return a default
-			var that = this;
+            fetch: function() {
+                // Return a default
+                var that = this;
 
-			this.stub = this.add({
-				responseTo: this.models[this.length-1],
-				message:"...", stub: true
-			});
-			
-            return $.Deferred(function(dfd){
-				setTimeout(function(){
-					var response = fakeServer.fakeServerDecideWhichToShow(that);
-					if(response){
+                this.stub = this.add({
+                    responseTo: this.models[this.length - 1],
+                    message: "...",
+                    stub: true
+                });
 
-						// Modify the elipsis stuf if it exists
-						var stub = that.findWhere({stub:true});
+                return $.Deferred(function(dfd) {
+                    setTimeout(function() {
+                        var response = fakeServer.fakeServerDecideWhichToShow(that);
+                        if (response) {
 
-						if(stub){
-							var settings = _.extend(stub.attributes, response);
-							settings.stub = false;
-							stub.set(settings).trigger('change:stub');
-							dfd.resolve(stub);
-						}else{
-							dfd.reject({error:"No stub found"});
-							return;
-						}
+                            // Modify the elipsis stuf if it exists
+                            var stub = that.findWhere({
+                                stub: true
+                            });
 
-						// Prompt if user input is required, fetch again if bot, otherwise the discussion is over
-						if(ConversationItemModel.requiresResponseFromUser(response.requiresResponseType)){
-							that.trigger('prompt', stub);
-						}else if(ConversationItemModel.requiresResponseFromBot(response.requiresResponseType)){
-							that.fetch().fail(function(error){
-								console.error(error);
-							});
-						}else{
-							// end of discussion
-						}
+                            if (stub) {
+                                var settings = _.extend(stub.attributes, response);
+                                settings.stub = false;
+                                stub.set(settings).trigger('change:stub');
+                                dfd.resolve(stub);
+                            } else {
+                                dfd.reject({
+                                    error: "No stub found"
+                                });
+                                return;
+                            }
 
-					}else{
-						dfd.reject({error:"Nothing found in the db"});
-					}
+                            // Prompt if user input is required, fetch again if bot, otherwise the discussion is over
+                            if (ConversationItemModel.requiresResponseFromUser(response.requiresResponseType)) {
+                                that.trigger('prompt', stub);
+                            } else if (ConversationItemModel.requiresResponseFromBot(response.requiresResponseType)) {
+                                that.fetch().fail(function(error) {
+                                    console.error(error);
+                                });
+                            } else {
+                                // end of discussion
+                            }
 
-				}, fakeServer.getVariableDelayTime());
-            }).promise();
+                        } else {
+                            dfd.reject({
+                                error: "Nothing found in the db"
+                            });
+                        }
 
-		},
-		model: ConversationItemModel,
-	},{
-		getInstance: function(){
-			return instance;
-		}
-	});
+                    }, fakeServer.getVariableDelayTime());
+                }).promise();
 
-	var instance = new ConversationItemModelCollection();
-	return ConversationItemModelCollection;
+            },
+            model: ConversationItemModel,
+        }, {
+            getInstance: function() {
+                return instance;
+            }
+        });
 
-});
+        var instance = new ConversationItemModelCollection();
+        return ConversationItemModelCollection;
+
+    });
